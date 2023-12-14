d54540ac63b5dd9798c74d4f0b5c70c388a3ce00
Bjarne Christel <develop.christel@gmail.com>
2023-12-14 10:15:30 +0100

    add: initialize spring project

---
+++ test-management/test-management/src/test/java/fse/testmanagement/TestManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fse.testmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
