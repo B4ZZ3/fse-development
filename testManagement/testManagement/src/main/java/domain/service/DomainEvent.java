4550ac0be93b97977805db83ae704839770b633d
Bjarne Christel <develop.christel@gmail.com>
2023-12-16 11:58:02 +0100

    [testmanager] add: DomainEvent

--- testManagement/testManagement/src/main/java/domain/service/DomainEvent.java
+++ testManagement/testManagement/src/main/java/domain/service/DomainEvent.java
@@ -2,4 +2,20 @@ package domain.service;
 
 public class DomainEvent {
 
+	String event;
+	Object object;
+	
+	public DomainEvent(String event, Object object) {
+		super();
+		this.event = event;
+		this.object = object;
+	}
+
+	public String getEvent() {
+		return event;
+	}
+
+	public Object getObject() {
+		return object;
+	}
 }
