81890e8c020bbcff9e4b53f0682549d279e7837e
Bjarne Christel <develop.christel@gmail.com>
2023-12-16 16:08:45 +0100

    [testmanager] upd: change get calls with id

--- testManagement/testManagement/src/main/java/domain/service/ChangeStatus.java
+++ testManagement/testManagement/src/main/java/domain/service/ChangeStatus.java
@@ -6,6 +6,7 @@ import application.IUserStoryRepo;
 import domain.model.StoryStatus;
 import domain.model.TestCase;
 import domain.model.UserStory;
+import domain.model.UserStoryId;
 
 public class ChangeStatus {
 
@@ -19,23 +20,25 @@ public class ChangeStatus {
 		this.messageQueue = messageQueue;
 	}
 	
-	public boolean changeStatus(UserStory userStory) {
+	public boolean changeStatus(int id) {
 		
 		boolean findAllTestCases = true;
 		
-		for(TestCase testCase: testCaseRepo.findByUserStoryId(userStory.getUserStoryId())) {		
+		for(TestCase testCase: testCaseRepo.findByUserStoryId(new UserStoryId(id))) {		
 			if (testCase == null) {
  	 			findAllTestCases = false;
  	 		}
 		}
 		
 		if(findAllTestCases) {
-			for(TestCase testCase: testCaseRepo.findByUserStoryId(userStory.getUserStoryId())) {
+			for(TestCase testCase: testCaseRepo.findByUserStoryId(new UserStoryId(id))) {
 				testCase.changeTestStatus(StoryStatus.READY_FOR_TEST);
 				testCaseRepo.save(testCase);
 			}
 			
+			UserStory userStory = userStoryRepo.findById(new UserStoryId(id));
 			userStory.changeStoryStatus(StoryStatus.READY_FOR_TEST);
+			userStoryRepo.save(userStory);
 			
 			DomainEvent statusChanged = new DomainEvent("statusChanged", userStory);
 			messageQueue.send(statusChanged);
