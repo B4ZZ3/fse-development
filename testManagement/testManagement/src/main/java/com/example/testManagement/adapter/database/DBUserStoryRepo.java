0c724bed745e7538f3488990db901f5dabc96e82
Bjarne Christel <develop.christel@gmail.com>
2023-12-16 17:06:14 +0100

    [testmanager] add: database handling for user stiry

--- testManagement/testManagement/src/main/java/com/example/testManagement/adapter/database/DBUserStoryRepo.java
+++ testManagement/testManagement/src/main/java/com/example/testManagement/adapter/database/DBUserStoryRepo.java
@@ -1,21 +1,36 @@
 package com.example.testManagement.adapter.database;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.example.testManagement.application.IUserStoryRepo;
 import com.example.testManagement.domain.model.UserStory;
 import com.example.testManagement.domain.model.UserStoryId;
 
+@Component
 public class DBUserStoryRepo implements IUserStoryRepo{
+	
+	private final JDBCUserStoryEntityRepo jdbcUserStoryEntityRepo;
+	
+	@Autowired
+	public DBUserStoryRepo(JDBCUserStoryEntityRepo jdbcUserStoryEntityRepo) {
+		this.jdbcUserStoryEntityRepo = jdbcUserStoryEntityRepo;
+	}
 
 	@Override
 	public UserStory findById(UserStoryId userStoryId) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<UserStoryEntity> userStoryEntity = jdbcUserStoryEntityRepo.findById(userStoryId.getId());
+        if (userStoryEntity.isPresent()) {
+            return userStoryEntity.get().toDomain();
+        } else {
+            return null;
+        }
 	}
 
 	@Override
 	public void save(UserStory userStory) {
-		// TODO Auto-generated method stub
-		
+		jdbcUserStoryEntityRepo.save(new UserStoryEntity(userStory));
 	}
-
 }
