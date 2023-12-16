312bde5f53bad0fcbc98278e80c30a046d88f693
Bjarne Christel <develop.christel@gmail.com>
2023-12-16 11:55:14 +0100

    [testmanager] add: string converter for enum StoryStatus

--- testManagement/testManagement/src/main/java/domain/model/StoryStatus.java
+++ testManagement/testManagement/src/main/java/domain/model/StoryStatus.java
@@ -1,6 +1,17 @@
 package domain.model;
 
 public enum StoryStatus {
-	IN_PROGRESS,
-	READY_FOR_TEST
+	IN_PROGRESS("in progress"),
+	READY_FOR_TEST("ready for test");
+	
+	private String statusName;
+	
+	private StoryStatus(String status) {
+		this.statusName = status;
+	}
+	
+	@Override
+	public String toString() {
+		return statusName;
+	}
 }
