dad405ae50103be7d5428a6aaef93911a0a752b6
Bjarne Christel <develop.christel@gmail.com>
2023-12-16 13:29:22 +0100

    [testmanager] upd: function to get all testCAses by UserStoryId

--- testManagement/testManagement/src/main/java/application/ITestCaseRepo.java
+++ testManagement/testManagement/src/main/java/application/ITestCaseRepo.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.util.Collection;
+
 import domain.model.TestCase;
 import domain.model.TestCaseId;
+import domain.model.UserStoryId;
 
 public interface ITestCaseRepo {
 	
 	public TestCase findById(TestCaseId testCaseId);
+	public Collection<TestCase> findByUserStoryId(UserStoryId userStoryId);
 	public void save(TestCase testCase);
 
 }
