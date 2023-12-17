89efca6932341fbcc6a79d559b33b7b894333468
Bjarne Christel <develop.christel@gmail.com>
2023-12-17 14:40:59 +0100

    [testing] add: event listener

--- testing/testing/src/main/java/com/example/testing/adapter/database/DBTestCaseRepo.java
+++ testing/testing/src/main/java/com/example/testing/adapter/database/DBTestCaseRepo.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.example.testManagement.adapter.database.JDBCTestCaseEntityRepo;
-import com.example.testManagement.adapter.database.TestCaseEntity;
 import com.example.testing.application.ITestCaseRepo;
 import com.example.testing.domain.model.TestCase;
 import com.example.testing.domain.model.TestCaseId;
